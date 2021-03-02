a64306a4338955975df1415d54fba401c0460b6e
José <jose@intuitivo.ai>
2021-03-01 23:16:15 -0300

    hey jude

--- src/views/MainView/MainView.tsx
+++ src/views/MainView/MainView.tsx
@@ -43,50 +43,6 @@ const MainView: React.FC = () => {
         },
     }))(Tooltip);
 
-    const getSocialMediaButtons = (size:ISize) => {
-        return SocialMediaData.map((data:ISocialMedia, index: number) => {
-            return <DarkTooltip
-                key={index}
-                disableFocusListener
-                title={data.tooltipMessage}
-                TransitionComponent={Fade}
-                TransitionProps={{ timeout: 600 }}
-                placement="left"
-            >
-                <div>
-                    <ImageButton
-                        buttonSize={size}
-                        image={data.imageSrc}
-                        imageAlt={data.imageAlt}
-                        href={data.href}
-                    />
-                </div>
-            </DarkTooltip>
-        });
-    };
-
-    const getEditorFeatureTiles = () => {
-        return EditorFeatureData.map((data:IEditorFeature) => {
-            return <div
-                className="EditorFeaturesTiles"
-                key={data.displayText}
-            >
-                <div
-                    className="EditorFeaturesTilesWrapper"
-                >
-                    <img
-                        draggable={false}
-                        alt={data.imageAlt}
-                        src={data.imageSrc}
-                    />
-                    <div className="EditorFeatureLabel">
-                        {data.displayText}
-                    </div>
-                </div>
-            </div>
-        });
-    };
-
     return (
         <div className={getClassName()}>
             <div className="Slider" id="lower">
